0c74aaf444bdf78bc2b19bcf894c07f027acac15
Anmol Dhiman <73706789+Anmoldh@users.noreply.github.com>
2021-07-19 11:32:30 +0530

    Better Speedtest

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -1,48 +1,51 @@
-import speedtest
-import bot
-
+from speedtest import Speedtest
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot import dispatcher, AUTHORIZED_CHATS
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
-from telegram.ext import CallbackContext, Filters, CommandHandler
+from telegram import Update, ParseMode
+from telegram.ext import Filters, CommandHandler
 
 
-
-def speedtst(update, context):
+def speedtest(update, context):
     message = update.effective_message
-    ed_msg = message.reply_text("Running Speed Test . . . 💨")
-    test = speedtest.Speedtest()
+    ed_msg = message.reply_text("Running Speed Test . . . ")
+    test = Speedtest()
     test.get_best_server()
     test.download()
     test.upload()
     test.results.share()
     result = test.results.dict()
-    context.bot.editMessageText(
-        "🔻 Download Speed : "
-        f"{speed_convert(result['download'])}\n"
-        "🔺 Upload Speed : "
-        f"{speed_convert(result['upload'])}\n"
-        "📶 Ping : "
-        f"{result['ping']}\n"
-        "🏬 ISP : "
-        f"{result['client']['isp']}",
-        update.effective_chat.id,
-        ed_msg.message_id,
-    )
+    path = (result['share'])
+    string_speed = f'''
+<b>Server</b>
+<b>Name:</b> <code>{result['server']['name']}</code>
+<b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+<b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
+<b>ISP:</b> <code>{result['client']['isp']}</code>
+<b>SpeedTest Results</b>
+<b>Upload:</b> <code>{speed_convert(result['upload'] / 8)}</code>
+<b>Download:</b>  <code>{speed_convert(result['download'] / 8)}</code>
+<b>Ping:</b> <code>{result['ping']} ms</code>
+<b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+'''
+    ed_msg.delete()
+    try:
+        update.effective_message.reply_photo(path, string_speed, parse_mode=ParseMode.HTML)
+    except:
+        update.effective_message.reply_text(string_speed, parse_mode=ParseMode.HTML)
 
 def speed_convert(size):
     """Hi human, you can't read bytes?"""
     power = 2 ** 10
     zero = 0
-    units = {0: "", 1: "Kb/s", 2: "Mb/s", 3: "Gb/s", 4: "Tb/s"}
+    units = {0: "", 1: "Kb/s", 2: "MB/s", 3: "Gb/s", 4: "Tb/s"}
     while size > power:
         size /= power
         zero += 1
     return f"{round(size, 2)} {units[zero]}"
 
 
-SPEED_HANDLER = CommandHandler(BotCommands.SpeedCommand, speedtst, 
-                                     filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+SPEED_HANDLER = CommandHandler(BotCommands.SpeedCommand, speedtest, 
+                                    filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
 
 dispatcher.add_handler(SPEED_HANDLER)
